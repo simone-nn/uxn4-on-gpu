0067ed3bb2bc44a57ffac22f596a976ae4a41ba8
andrei <andrei.ghita2002@gmail.com>
2024-12-24 17:32:48 +0100

    shaders: compiled compute shader

--- compile_shaders.sh
+++ compile_shaders.sh
@@ -5,7 +5,6 @@ cd shaders || exit
 glslc shader.vert -o vert.spv
 glslc shader.frag -o frag.spv
 glslc shader.comp -o comp.spv
-echo "Note: compute shader not ready to be compiled!"
 
 cd ..
 echo "Shaders compiled successfully!"
