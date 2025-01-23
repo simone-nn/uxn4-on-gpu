e87942793da62c9e8c542be5a9074dfa7d96b8ed
andrei <andrei.ghita2002@gmail.com>
2025-01-23 13:09:54 +0000

    Shaders: started implementing the UXN VM as a shader

--- compile_shaders.sh
+++ compile_shaders.sh
@@ -5,6 +5,7 @@ cd shaders || exit
 glslc shader.vert -o vert.spv
 glslc shader.frag -o frag.spv
 glslc shader.comp -o comp.spv
+glslc uxn_emu.comp -o uxn_emu.spv
 
 cd ..
 echo "Shaders compiled successfully!"
