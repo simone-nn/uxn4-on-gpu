6b9ead4c8a026bd2de271c0c4065e55db226b74d
andrei <andrei.ghita2002@gmail.com>
2025-02-05 13:23:08 +0000

    upgraded API version to 1.3 and fixed the bug

--- compile_shaders.sh
+++ compile_shaders.sh
@@ -6,7 +6,8 @@ glslc shader.vert -o vert.spv
 glslc shader.frag -o frag.spv
 glslc shader.comp -o comp.spv
 glslc uxn_emu.comp -o uxn_emu.spv
-glslc simple.comp -o simple.spv
+#glslc simple.comp -o simple.spv
+glslangValidator -V --target-env vulkan1.3 simple.comp -o simple.spv
 
 cd ..
 echo "Shaders compiled successfully!"
