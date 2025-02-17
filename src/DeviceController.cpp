3d0653889af3f40ff906b8e175f556a5a8d9465c
andrei <andrei.ghita2002@gmail.com>
2025-02-17 15:49:29 +0000

    texture sampler

--- src/DeviceController.cpp
+++ src/DeviceController.cpp
@@ -394,7 +394,9 @@ private:
     VkDeviceMemory hostStagingMemory;
 
     VkImage textureImage;
+    VkImageView textureImageView;
     VkDeviceMemory textureImageMemory;
+    VkSampler textureSampler;
 
     std::vector<VkSemaphore> imageAvailableSemaphores;
     std::vector<VkSemaphore> renderFinishedSemaphores;
@@ -821,15 +823,57 @@ private:
         copyBufferToImage(ctx, stagingBuffer, textureImage, static_cast<uint32_t>(textureWidth), static_cast<uint32_t>(textureHeight));
         transitionImageLayout(ctx, textureImage, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
 
+        // creating the image view object
+        VkImageViewCreateInfo viewInfo{};
+        viewInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+        viewInfo.image = textureImage;
+        viewInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+        viewInfo.format = VK_FORMAT_R8G8B8A8_SRGB;
+        viewInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+        viewInfo.subresourceRange.baseMipLevel = 0;
+        viewInfo.subresourceRange.levelCount = 1;
+        viewInfo.subresourceRange.baseArrayLayer = 0;
+        viewInfo.subresourceRange.layerCount = 1;
+        if (vkCreateImageView(ctx.device, &viewInfo, nullptr, &textureImageView) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create texture image view!");
+        }
+
         // freeing stuff
         stbi_image_free(pixels);
         vkDestroyBuffer(ctx.device, stagingBuffer, nullptr);
         vkFreeMemory(ctx.device, stagingBufferMemory, nullptr);
     }
 
-    void initVertexBuffer() {
-        std::cout << "..initVertexBuffer" << std::endl;
-        //todo
+    void initTextureSampler() {
+        std::cout << "..initTextureSampler" << std::endl;
+
+        VkPhysicalDeviceProperties properties{};
+        vkGetPhysicalDeviceProperties(ctx.physicalDevice, &properties);
+
+        VkSamplerCreateInfo samplerInfo{};
+        samplerInfo.sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
+        samplerInfo.magFilter = VK_FILTER_LINEAR;
+        samplerInfo.minFilter = VK_FILTER_LINEAR;
+        samplerInfo.addressModeU = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+        samplerInfo.addressModeV = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+        samplerInfo.addressModeW = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+        // the higher this is the slower it is for performance
+        // samplerInfo.anisotropyEnable = VK_TRUE;
+        // samplerInfo.maxAnisotropy = std::min(2.0f, properties.limits.maxSamplerAnisotropy);
+        samplerInfo.anisotropyEnable = VK_FALSE;
+        samplerInfo.maxAnisotropy = 1.0f;
+        samplerInfo.borderColor = VK_BORDER_COLOR_INT_OPAQUE_BLACK;
+        samplerInfo.unnormalizedCoordinates = VK_FALSE;
+        samplerInfo.compareEnable = VK_FALSE;
+        samplerInfo.compareOp = VK_COMPARE_OP_ALWAYS;
+        samplerInfo.mipmapMode = VK_SAMPLER_MIPMAP_MODE_LINEAR;
+        samplerInfo.mipLodBias = 0.0f;
+        samplerInfo.minLod = 0.0f;
+        samplerInfo.maxLod = 0.0f;
+
+        if (vkCreateSampler(ctx.device, &samplerInfo, nullptr, &textureSampler) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create texture sampler!");
+        }
     }
 
     void initDescriptorPool() {
@@ -1106,7 +1150,7 @@ private:
         initDescriptorPool();
         initResources();
         initTexture();
-        initVertexBuffer();
+        initTextureSampler();
         initComputePipeline();
         initFrameBuffers();
         initGraphicsPipeline();
@@ -1301,6 +1345,8 @@ private:
 
     void cleanup() {
         delete uxn;
+        vkDestroySampler(ctx.device, textureSampler, nullptr);
+        vkDestroyImageView(ctx.device, textureImageView, nullptr);
         vkDestroyImage(ctx.device, textureImage, nullptr);
         vkFreeMemory(ctx.device, textureImageMemory, nullptr);
         vkDestroyBuffer(ctx.device, hostStagingBuffer, nullptr);
