42e00ab8c03230c4d187c2c8f3f37d591f067df9
andrei <andrei.ghita2002@gmail.com>
2025-02-19 16:54:46 +0000

    compile_shaders.sh: now also does the blit shader

--- compile_shaders.sh
+++ compile_shaders.sh
@@ -2,24 +2,27 @@
 
 OUTPUT_DIR="cmake-build-debug"
 SHADER_DIR="shaders"
-
-cd ../${SHADER_DIR} || exit
-
-#glslc shader.vert -o vert.spv
-#glslc shader.frag -o frag.spv
-
-# Compile
-glslangValidator -V --target-env vulkan1.2 uxn_emu.comp -o uxn_emu.spv
-
-# Patch the shader
-spirv-as  --target-env vulkan1.2 <(sed -f shader_patch.sed <(spirv-dis uxn_emu.spv)) -o uxn_emu.spv
-
-spirv-dis uxn_emu.spv -o uxn_emu.spv.txt
+UXN_SHADERS="uxn_emu blit"
 
 cd ..
-
-# Copy shader to binary directory
 mkdir -p ./${OUTPUT_DIR}/shaders
-cp ./${SHADER_DIR}/uxn_emu.spv ./${OUTPUT_DIR}/shaders/uxn_emu.spv
+cd ${SHADER_DIR} || exit
+
+for shader in $UXN_SHADERS; do
+  echo "Compiling $shader"
+    # Compile
+    glslangValidator -V --target-env vulkan1.2 "${shader}".comp -o "${shader}".spv
+
+    # Patch the shader
+    spirv-as  --target-env vulkan1.2 <(sed -f shader_patch.sed <(spirv-dis "${shader}".spv)) -o "${shader}".spv
+
+    # storing the spir-v disassembly
+    spirv-dis "${shader}".spv -o "${shader}".spv.txt
+
+    # Copy shader to binary directory
+    cp "${shader}".spv ../${OUTPUT_DIR}/shaders/uxn_emu.spv
+done
+
+cd ..
 
 echo "Shaders compiled successfully!"
