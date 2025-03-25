43251ba864fb2cb5a14f0176a66631e9bcb335b0
andrei <andrei.ghita2002@gmail.com>
2025-03-25 18:06:18 +0000

    vertex shader: inPosition -> vertex

--- shaders/shader.vert.glsl
+++ shaders/shader.vert.glsl
@@ -1,9 +1,9 @@
 #version 450
-layout(location = 6) in vec4 inPosition;
+layout(location = 6) in vec4 vertex;
 
 layout(location = 0) out vec2 fragUV;
 
 void main() {
-    fragUV = inPosition.zw;
-    gl_Position = vec4(inPosition.xy, 0.0, 1.0);
+    fragUV = vertex.zw;
+    gl_Position = vec4(vertex.xy, 0.0, 1.0);
 }
